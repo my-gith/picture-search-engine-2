5e4906769a64d523d5baad2e748cad3ae8a291d3
my-gith <mylogin2github>
2019-02-24 15:02:55 +0200

    add LESSON_32

---
+++ LESSON_32/js/script.js
@@ -0,0 +1,54 @@
+$(function(){
+
+	$('.search form').submit(function() {
+		var search = $(this).find('input[name="search"]').val();
+
+		alert(search);
+
+
+	 	$.ajax({
+			url: 'http://foxk.ru/search.php?count=3&string='+search,
+			type:"GET",//POST/GET
+			crossDomain : true,
+			async: false,
+			dataType: "json",
+			//data: {//для метода POST
+			//string: search,
+			//count:10
+			//}
+			success: function(json) {
+			$( "div" ).remove( ".items" );
+			console.log(json);
+			//json.data.result.items
+
+				for (var i = 0; i <json.data.result.items.length; i++) {
+					// console.log (json.data.result.items[i].media);
+
+					var title = json.data.result.items[i].title;
+					var full = json.data.result.items[i].media;
+					var thumb = json.data.result.items[i].thumbnail;
+
+				
+					$('.content').append('<div class="items"><img src="'+full+'"><div class="img_option"><p class="name">'+title+'</p>');
+				}
+			}
+		});
+
+		return false;
+	});
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
